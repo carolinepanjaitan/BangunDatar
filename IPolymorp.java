7b28b47d92affe2642e51ef5e93fd6fd68069616
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-06-02 17:49:12 +0700

    Tugas

---
+++ IPolymorp.java
@@ -0,0 +1,11 @@
+package com.bangundatar.interfes;
+
+public class IPolymorp {
+    void poly (IBangunDatar bangundatar){
+        bangundatar.hitungLuas();
+        bangundatar.hitungKeliling();
+        bangundatar.volume();
+
+    }
+}
+    
